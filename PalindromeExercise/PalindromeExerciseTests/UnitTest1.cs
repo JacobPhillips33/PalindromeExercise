09f7b176cacb12752ba4b8140838961b713cf2bd
Jacob Phillips <jakephillips.wv@gmail.com>
2022-10-03 15:05:55 -0500

    initial commit

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,23 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("nope", false)]
+        [InlineData("hannah", true)]
+        [InlineData("madam", true)]
+        [InlineData("sorry, not this one", false)]
+        [InlineData("try again", false)]
+        public void IsAPalindromeTest(string word, bool expected)
         {
+            //Arrange
+            var palindromeTester = new WordSmith();
 
+            //Act
+            var actual = palindromeTester.IsAPalindrome(word);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
